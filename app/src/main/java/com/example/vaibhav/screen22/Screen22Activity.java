f9da7846267701b2b0fcb95a732bc87c3689f947
wsdesign <info.wsdesignuiux@gmail.com>
2019-01-10 17:04:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vaibhav/screen22/Screen22Activity.java
@@ -0,0 +1,13 @@
+package com.example.vaibhav.screen22;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Screen22Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen22);
+    }
+}
